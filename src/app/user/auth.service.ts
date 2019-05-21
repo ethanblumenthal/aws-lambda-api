ea8705e42f7db253613d35783e60a7e410f48909
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-05-20 20:19:54 -0500

    completed signup functionality

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -5,13 +5,24 @@ import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
+import { CognitoUserPool, CognitoUserAttribute, CognitoUser } from 'amazon-cognito-identity-js';
+
 import { User } from './user.model';
+
+const POOL_DATA = {
+  UserPoolId: 'us-east-2_yPVhoai4G',
+  ClientId: '1gdei1v9vsmg0rd6h9iueln1rp'
+};
+
+const userPool = new CognitoUserPool(POOL_DATA);
 
 @Injectable()
 export class AuthService {
   authIsLoading = new BehaviorSubject<boolean>(false);
   authDidFail = new BehaviorSubject<boolean>(false);
   authStatusChanged = new Subject<boolean>();
+  registeredUser: CognitoUser;
+
   constructor(private router: Router) {}
   signUp(username: string, email: string, password: string): void {
     this.authIsLoading.next(true);
@@ -20,17 +31,42 @@ export class AuthService {
       email: email,
       password: password
     };
+    const attrList: CognitoUserAttribute[] = [];
     const emailAttribute = {
       Name: 'email',
       Value: user.email
     };
+    attrList.push(new CognitoUserAttribute(emailAttribute));
+    userPool.signUp(user.username, user.password, attrList, null, (err, res) => {
+      if (err) {
+        this.authDidFail.next(true)
+        this.authIsLoading.next(true)
+        return;
+      }
+        this.authDidFail.next(false)
+        this.authIsLoading.next(false)
+        this.registeredUser = res.user;
+    })
     return;
   }
   confirmUser(username: string, code: string) {
     this.authIsLoading.next(true);
     const userData = {
       Username: username,
+      Pool: userPool
     };
+    const cognitoUser = new CognitoUser(userData);
+    cognitoUser.confirmRegistration(code, true, (err, res) => {
+      if (err) {
+        this.authDidFail.next(true);
+        this.authIsLoading.next(false);
+        return;
+      } else {
+        this.authDidFail.next(false);
+        this.authIsLoading.next(false);
+        this.router.navigate(['/']);
+      }
+    });
   }
   signIn(username: string, password: string): void {
     this.authIsLoading.next(true);
